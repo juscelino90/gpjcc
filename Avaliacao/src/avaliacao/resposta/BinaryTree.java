1e38609d0781ceb5cbb4c53ea1388332c14b39bd
juscelino90 <40297510+juscelino90@users.noreply.github.com>
2018-06-18 19:35:21 -0300

    Revert "ok"
    This reverts commit 5c247498d505af41ab0e9ab9167e66013538130e.

--- Avaliacao/src/avaliacao/resposta/BinaryTree.java
+++
@@ -1,7 +0,0 @@
-package avaliacao.resposta;
-
-public class BinaryTree { 
-	int valor;
-	BinaryTree left = null;
-	BinaryTree right = null;
-}
--- Avaliacao/src/avaliacao/resposta/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author juscelinocoelho
- *
- */
-package avaliacao.resposta;
